f934faba796c238cb1a8c53add712be488c9799d
wilbret17 <wille.nordell@gmail.com>
2024-11-23 16:47:17 +0200

    3.8 done, (shows data in HTTP POST request)

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
+morgan.token('post-data', (req) => {
+    return req.method === 'POST' ? JSON.stringify(req.body) : '';
+
+});
+
+app.use(morgan('method :url :status :res[content-length] - :response-time ms :post-data'));
 app.use(bodyParser.json());
 
 const persons = [
