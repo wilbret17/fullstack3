1fee99ec3ebfef593694a3e2d1c0bbb21f95ddb7
wilbret17 <wille.nordell@gmail.com>
2024-11-23 15:35:25 +0200

    3.2 phonebook done

--- index.js
+++ index.js
@@ -12,6 +12,17 @@ app.get('/api/persons', (req, res) => {
     res.json(persons);
 });
 
+app.get('/info', (req, res) => {
+    const currentTime = new Date();
+    const totalEntries = persons.length;
+
+    res.send(`
+        <p>Phonebook has info for ${totalEntries} people</p>
+        <p>${currentTime}</p>
+    `);
+});
+
+
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
