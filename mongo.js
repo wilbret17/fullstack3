4566a65c345a0412c82239d658141d5e6f11a393
wilbret17 <wille.nordell@gmail.com>
2024-11-26 19:39:24 +0200

    3.12 MongoDB added and tested

---
+++ mongo.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+const args = process.argv;
+
+if (args.length < 3) {
+  console.log('Usage: node mongo.js <password> [<name> <number>]');
+  process.exit(1);
+}
+
+const password = args[2];
+const name = args[3];
+const number = args[4];
+
+const url = process.env.MONGODB_URI; 
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    
+    if (args.length === 3) {
+      Person.find({})
+        .then((result) => {
+          console.log('phonebook:');
+          result.forEach((person) => console.log(`${person.name} ${person.number}`));
+          mongoose.connection.close(); 
+        })
+        .catch((err) => {
+          console.error('Error fetching data:', err);
+          mongoose.connection.close(); 
+        });
+    } else if (args.length === 5) {
+      const person = new Person({ name, number });
+      person.save()
+        .then(() => {
+          console.log(`Added ${name} number ${number} to phonebook`);
+          mongoose.connection.close(); 
+        })
+        .catch((err) => {
+          console.error('Error saving person:', err);
+          mongoose.connection.close(); 
+        });
+    }
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+  });
