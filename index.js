eb1e7f9696080c86de693b9b1d3b3f6a88e0ecb3
wilbret17 <wille.nordell@gmail.com>
2024-11-28 20:29:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
-}));
+app.use(cors());
+
 
 
 morgan.token('post-data', (req) => {
