32ae19d441b1a950e9819c84aed05a6e903ce1d7
wilbret17 <wille.nordell@gmail.com>
2024-11-29 13:25:16 +0200

    Update person.js

--- models/person.js
+++ models/person.js
@@ -1,19 +1,36 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
+require('dotenv').config(); 
 
 mongoose.set('strictQuery', false);
 
 const url = process.env.MONGODB_URI;
 
+if (!url) {
+  console.error('Error: MONGODB_URI is not defined in the .env file');
+  process.exit(1);
+}
+
 console.log('Connecting to', url);
 
-mongoose.connect(url)
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log('Connected to MongoDB'))
-  .catch((error) => console.log('Error connecting to MongoDB:', error.message));
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
+    process.exit(1); 
+  });
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  number: {
+    type: String,
+    required: [true, 'Number is required'],
+  },
 });
 
 personSchema.set('toJSON', {
